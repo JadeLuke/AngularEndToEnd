30f01eef76b1ec755a26eaed26a3c518dbd82f71
JadeLuke <jadeluke.booysen@gmail.com>
2025-01-20 16:10:25 +0200

    added product-list html and ts

---
+++ client/product-crud/src/app/components/product-list/product-list/product-list.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Product } from '../../../models/product.model';
+import { ProductService } from '../../../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = []
+
+  constructor(private productService: ProductService) {
+    
+  }
+  ngOnInit(): void {
+    this.loadProducts()
+  }
+
+  loadProducts(): void{
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.products = data
+      },
+      error: (e) => console.error(e),
+    })
+  }
+
+  deleteProduct(id: number): void {
+    if(confirm('Are you sure you want to delete this product?')){
+      this.productService.delete(id).subscribe({
+        next: () => {
+          this.loadProducts()
+        },
+        error: (e) => console.error(e)
+      })
+    }
+  }
+
+
+
+}
